4d9f2ae0c32233b552b1a2273f8e7687f8d6f71b
Luke Granger <grangerl330@gmail.com>
2019-01-17 16:32:16 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,12 +14,12 @@ require 'pry'
   #   }
   # }
   
+
+
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| ice_cream == "strawberry"}
-     end 
-    end 
-  end 
+ contacts["Freddy Mercury"].each do |charcter, value |
+     if charcter == :favorite_ice_cream_flavors
+       value.delete_if  {|flavor|  flavor == "strawberry"}
+     end
+ end
 end
